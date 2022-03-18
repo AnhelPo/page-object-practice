5a8b277f82f5da6b6c726cd18ba45118b441b10f
AnhelPo <annchista@yahoo.com>
2022-03-18 16:22:35 +0300

    Refactor test_main_page.py
    Now main page is an instance of MainPage class.
    Test uses class methods.

--- test_main_page.py
+++ test_main_page.py
@@ -4,11 +4,14 @@ Checks out:
     - guest user can go to login page from the main page.
 '''
 
-from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
+
+
+URL = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    LINK = "http://selenium1py.pythonanywhere.com/"
-    browser.get(LINK)
-    login_link = browser.find_element(By.ID, "login_link")
-    login_link.click()
+    URL = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, URL)
+    page.open()
+    page.go_to_login_page()
