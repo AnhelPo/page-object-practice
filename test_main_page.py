4fa2e727272a6eb0b3167afff1b851c82139cb12
AnhelPo <annchista@yahoo.com>
2022-03-18 17:10:11 +0300

    Add the check for existence of an element
    To BasePage class (base_page.py) add:
     - method for checking, if element exists (returns boolean),
     - handling of NoSuchElementException.
    To MainPage class (main_page.py) add:
     - call of BasePage method for checking,
     - an assert statement.
    To test_main_page.py add:
     - call of MainPage method for checking.

--- test_main_page.py
+++ test_main_page.py
@@ -6,12 +6,11 @@ Checks out:
 
 from pages.main_page import MainPage
 
-
 URL = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    URL = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, URL)
     page.open()
     page.go_to_login_page()
+    page.should_be_login_link()
