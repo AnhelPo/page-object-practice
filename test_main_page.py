59eea5e00a267b4e50973bbd5ca92189d66d098b
AnhelPo <annchista@yahoo.com>
2022-03-19 08:59:30 +0300

    Add to the main page test an extended check for correctness of login link
    When a guest user on the main page clicks on the login link,
    a correct login page should be opened. Login page should contain
    correct forms for log in and registration.

--- test_main_page.py
+++ test_main_page.py
@@ -2,16 +2,21 @@
 Tests for the main page.
 Checks:
     - login link exists,
-    - guest user can go to the login page from the main page.
+    - guest user can go to the login page from the main page,
+    - login page has correct URL and contains correct forms for
+      log in and registration.
 '''
 
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 URL = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, URL)
-    page.open()
-    page.should_be_login_link()
-    page.go_to_login_page()
+    main_page = MainPage(browser, URL)
+    main_page.open()
+    main_page.should_be_login_link()
+    main_page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
