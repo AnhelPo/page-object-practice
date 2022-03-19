5008bfb7c6796b04a32d9bb99d4e7d0e19fe670c
AnhelPo <annchista@yahoo.com>
2022-03-19 10:25:21 +0300

    Add comments to function's calls

--- test_main_page.py
+++ test_main_page.py
@@ -16,7 +16,12 @@ URL = "http://selenium1py.pythonanywhere.com/"
 def test_guest_can_go_to_login_page(browser):
     main_page = MainPage(browser, URL)
     main_page.open()
+    # check for login link existence on the main page
     main_page.should_be_login_link()
+    # go to the login page
     main_page.go_to_login_page()
+
+    # browser.current_url has changed to login page URL
     login_page = LoginPage(browser, browser.current_url)
+    # check for correct URL and forms for log in and registration
     login_page.should_be_login_page()
