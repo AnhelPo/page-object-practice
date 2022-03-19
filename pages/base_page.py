1d686ffc789a9fdbc3d439fd41a14934e207e103
AnhelPo <annchista@yahoo.com>
2022-03-19 14:20:19 +0300

    Add method for Stepik quiz
    Method solve_quiz_and_get_code() handles alerts:
     - from the first alert a number is recieved,
     - a number is handled by the math function,
     - result number is sent to the first alert as a key,
     - from the second alert a code for quiz is recieved,
     - code is displayed in the console.

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,12 +2,15 @@
 Base class for all pages in project.
 '''
 
+from math import log, sin
+
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException 
 
 
 class BasePage():
 
-    def __init__(self, browser, url, timeout=2):
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
@@ -21,3 +24,19 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+    # check for Stepik 4.3.2, alert proceeding
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(log(abs((12 * sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+    
