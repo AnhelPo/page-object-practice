d07172d51a27755f7ae650df57884e1e2744adc9
AnhelPo <annchista@yahoo.com>
2022-03-22 09:20:38 +0300

    Refactor login tests

--- test_login_page.py
+++ test_login_page.py
@@ -13,7 +13,17 @@ from pages.login_page import LoginPage
 URL = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 
 
-def test_url_is_correct_and_forms_for_login_and_registration_exist(browser):
+def get_class_instance_and_open_page(browser):
+    '''Precondition for all tests. Function is called from tests.'''
+
     page = LoginPage(browser, URL)
     page.open()
+    return page
+
+
+def test_url_is_correct_and_forms_for_login_and_registration_exist(browser):
+    '''Link for login contains the word "login". Forms for log in and
+    registration contain required fields and buttons.'''
+
+    page = get_class_instance_and_open_page(browser)
     page.should_be_login_page()
